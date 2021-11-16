69bf677c0df4a65e3e0826d6e5f2ba661fdb76cd
LrMgic <63558214+LrMgic@users.noreply.github.com>
2021-11-16 21:53:25 +0100

    V 7.0
    Ej. 6 Finalitzat
    
    Ej. 7 Base

---
+++ PEC4_Ej5/src/app/winelist/winelist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-winelist',
+  templateUrl: './winelist.component.html',
+  styleUrls: ['./winelist.component.scss']
+})
+export class WinelistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ PEC4_Ej7/src/app/wineitem/wineitem.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodDTO } from '../models/food';
+import { WineDTO } from '../models/wine';
+
+@Component({
+  selector: 'app-wineitem',
+  templateUrl: './wineitem.component.html',
+  styleUrls: ['./wineitem.component.scss'],
+})
+export class WineitemComponent implements OnInit {
+  public wineitem: WineDTO;
+  public foods: FoodDTO[] = [];
+  public foodPairing!: string;
+  public alavenda!: string;
+  public quantityNot!: boolean;
+  public quantity: number;
+  public quantities: any;
+  constructor() {
+    this.wineitem = new WineDTO(
+      'Viña esmeralda',
+      'assets/img/Vina_esmeralda_2019.png',
+      6.15,
+      false,
+      7
+    );
+    this.quantities = [
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ];
+    this.quantity = this.wineitem.quantityInCart;
+  }
+
+  empty() {
+    if (this.wineitem.quantityInCart < 1) {
+      this.quantityNot = false;
+    } else {
+      this.quantityNot = true;
+    }
+  }
+
+  ngOnInit() {
+    this.foods.push(new FoodDTO('Queso', 150, false, false));
+    this.foods.push(new FoodDTO('Pescado', 80, false, false));
+    this.foods.push(new FoodDTO('Pan', 180, true, true));
+    if (this.wineitem.isOnSale) {
+      this.alavenda = 'EN STOCK!';
+    } else {
+      this.alavenda = 'AGOTADO';
+    }
+    this.empty();
+  }
+
+  toggleDown() {
+    this.wineitem.quantityInCart = this.wineitem.quantityInCart - 1;
+    this.empty();
+  }
+
+  toggleUp() {
+    this.wineitem.quantityInCart = this.wineitem.quantityInCart + 1;
+    this.empty();
+  }
+
+  onSelect(quantity: number) {
+    this.wineitem.quantityInCart = quantity
+  }
+}
